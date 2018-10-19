236479a288e6e8ca55ffd7aae460ad20fd8034a1
Ryan Bachman <jacksprat455@gmail.com>
2018-10-19 11:57:54 -0500

    push

---
+++ js.js
@@ -0,0 +1,52 @@
+// number(1, 100);
+//
+// function number(firstNumber, secondNumber)
+// {
+//     if (firstNumber > secondNumber)
+//     {
+//         console.log(-1);
+//         return;
+//     }
+//     for (x = firstNumber; x < secondNumber; x++)
+//     {
+//         console.log(x);
+//     }
+// }
+
+
+
+// numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+//
+// for (var x=1; x<=10; x++){
+//     for (var n=0; n<numbers.length; n++){
+//         console.log(1 * numbers[n], 2 * numbers[n], 3 * numbers[n], 4 * numbers[n], 5 * numbers[n], 6 * numbers[n], 7 * numbers[n], 8 * numbers[n], 9 * numbers[n], 10 * numbers[n])
+//     }
+//     break;
+// }
+
+
+
+// numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+//
+// for (var x=1; x<=10; x++){
+//     for (var n=0; n<numbers.length; n++){
+//         console.log( 3 * numbers[n])
+//     }
+//     break;
+// }
+
+
+
+function max(num1, num2) {
+    for(var x=0; x<501; x++){
+
+    }
+
+}
+
+
+
+function isVowel(){
+    var string = prompt('Enter a letter: ')
+
+}
